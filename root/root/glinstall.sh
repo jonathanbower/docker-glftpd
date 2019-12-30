a53532f3d9d09cc1d44be823544c12c43441d930
Jonathan Bower <jonathan.bower@ericsson.com>
2019-12-30 20:34:11 +0100

    comment out DHPARAM_FILE from glftpd.conf

--- root/root/glinstall.sh
+++ root/root/glinstall.sh
@@ -20,18 +20,22 @@ cd ${tgz_name}
 # ^ bug on line 1251, //ftp-data
 
 # move glftpd.conf so it can easily be mounted on host
-mv /etc/glftpd.conf /glftpd/ftp-data/
+data_path='/glftpd/ftp-data'
+mv /etc/glftpd.conf $data_path/
 sed -i '/server_args/s/$/-r \/glftpd\/ftp-data\/glftpd.conf/' /etc/xinetd.d/glftpd
 echo "0  0 * * *      /glftpd/bin/reset -r /glftpd/ftp-data/glftpd.conf" | crontab -
 
 # change location of passwd and group so it can easily be mounted on host
-mv /glftpd/etc/passwd /glftpd/ftp-data/
-mv /glftpd/etc/group /glftpd/ftp-data/
-sed -i '/^datapath/a pwd_path        /ftp-data/passwd' /glftpd/ftp-data/glftpd.conf
-sed -i '/^pwd_path/a grp_path        /ftp-data/group' /glftpd/ftp-data/glftpd.conf
+mv /glftpd/etc/passwd $data_path/
+mv /glftpd/etc/group $data_path/
+sed -i '/^datapath/a pwd_path        /ftp-data/passwd' $data_path/glftpd.conf
+sed -i '/^pwd_path/a grp_path        /ftp-data/group' $data_path/glftpd.conf
 
 # change to static version since glibc 2.28 is required from version 2.10
 cp /glftpd/bin/glftpd-full-static /glftpd/bin/glftpd
 
 # disable IPV6
 sed -i 's/IPv6$//' /etc/xinetd.d/glftpd
+
+# comment out DHPARAM_FILE from glftpd
+sed -i '/^DHPARAM_FILE/s/^/#/g' $data_path/glftpd.conf
